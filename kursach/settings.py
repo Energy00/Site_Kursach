b3addca86f18a58573957929bdf5311f113c251e
Energy00 <newenglishmen.98@gmail.com>
2022-05-16 19:03:33 +0700

    try new

--- kursach/settings.py
+++ kursach/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,10 +51,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'kursach.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, "roof_order/templates")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- roof_order/apps.py
+++ roof_order/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RoofConstructionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'roof_construction'
+    name = 'roof_order'
