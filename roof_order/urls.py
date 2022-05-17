4cd1877297cd921a42ddd5a0ba1341727c7c33e7
Energy00 <newenglishmen.98@gmail.com>
2022-05-17 15:04:58 +0700

    main

--- roof_order/urls.py
+++ roof_order/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
 
 urlpatterns = [
-    re_path(r'^$', name='roof_order')
+    # re_path(r'^$', name='roof_order')
 ]
---
+++ roof_user/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RoofUserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'roof_user'
